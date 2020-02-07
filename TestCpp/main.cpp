273631b1499524fb1f250650f167984f1f00ff7f
Dominique Pavlin <dpavlin@i-ozo.com>
2020-02-07 12:22:16 +0100

    Object as map values

--- TestCpp/main.cpp
+++ TestCpp/main.cpp
@@ -11,38 +11,50 @@
 
 using namespace std;
 
+class Person{
+
+    private:
+    string name;
+    int age;
+    
+    public:
+    Person():name(""), age(0)
+    {
+        
+    }
+    
+    Person(const Person &other){
+        cout << "Copy constructor invoked" << endl;
+        name = other.name;
+        age = other.age;
+    }
+    
+    Person(string name,int age):name(name),age(age)
+    {
+        
+    }
+    
+    void print(){
+        cout << name << ": " << age << endl;
+    }
+};
+
+
 int main() {
 
-    map<string,int> ages;
+    map<int,Person> people;
     
-    ages["Mike"] = 40;
-    ages["Raj"] = 20;
-    ages["Vicky"] = 30;
-
-    pair<string,int> addToMap("Peter",100);
-    ages.insert(addToMap);
+    people[0] = Person("Mike",40);
+    people[1] = Person("Vicky",28);
+    people[2] = Person("Raj",31);
     
-    ages["Mike"] = 85;
+    people.insert(make_pair(55,Person("Bob",45)));
+    people.insert(make_pair(7,Person("Sue",52)));
     
-    cout << ages["Raj"] << endl;
-    if(ages.find("Vicky") != ages.end())
+    for(map<int,Person>::iterator it=people.begin();it!=people.end();it++)
     {
-        cout << "Found Vicky" << endl;
-    }
-    else
-    {
-        cout << "Key not found"<<endl;
-    }
-    
-    for(map<string,int>::iterator it=ages.begin();it != ages.end();it++)
-       {
-           pair<string,int> age = *it;
-           cout << age.first<<": "<<age.second<<endl;
-       }
-    cout <<endl;
-    for(map<string,int>::iterator it=ages.begin();it != ages.end();it++)
-    {
-        cout << it->first<<": "<<it->second<<endl;
+        cout << it->first << ": " << flush;
+        it->second.print();
     }
     
     return 0;
